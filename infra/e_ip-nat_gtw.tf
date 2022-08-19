ced58a62979078bbd473f61fc7d098ece99e3b00
issagueye <issacolygueye@gmail.com>
2022-08-19 15:33:18 +0200

    change rt_id on aws_route.main

--- infra/e_ip-nat_gtw.tf
+++ infra/e_ip-nat_gtw.tf
@@ -59,7 +59,7 @@ resource "aws_route_table_association" "nat_gtw_access" {
 # Add route to route table
 resource "aws_route" "main" {
   count = var.enable_nat_gateway == true ? local.nat_gateway_count : 0
-  route_table_id         = aws_vpc.my_vpc.main_route_table_id[count.index]
+  route_table_id         = aws_route_table.private-rt[count.index].id
   nat_gateway_id         = aws_nat_gateway.main[count.index].id
   destination_cidr_block = "0.0.0.0/0"
   depends_on = [
